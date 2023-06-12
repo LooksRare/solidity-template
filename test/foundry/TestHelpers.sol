81533570a130aac4cb0809fd45e64f701b899aa6
Hiroshi <108636367+0xhiroshi@users.noreply.github.com>
2023-06-12 15:04:16 +0100

    fix: Do not use precompile addresses as users (#15)

--- test/foundry/TestHelpers.sol
+++ test/foundry/TestHelpers.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.0;
 import {Test} from "../../lib/forge-std/src/Test.sol";
 
 abstract contract TestHelpers is Test {
-    address public user1 = address(1);
-    address public user2 = address(2);
-    address public user3 = address(3);
-    address public user4 = address(4);
+    address public user1 = address(11);
+    address public user2 = address(12);
+    address public user3 = address(13);
+    address public user4 = address(14);
+    address public owner = address(69);
 
     modifier asPrankedUser(address user) {
         vm.startPrank(user);
