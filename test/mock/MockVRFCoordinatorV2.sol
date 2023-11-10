f013af50717ca9de20fbf87fdcdfd53bd9fc0d9c
0xBunta <134520881+0xBunta@users.noreply.github.com>
2023-11-11 03:00:32 +0900

    feat: Invariant helpers and mockVRF (#26)
    * feat: Invariant helpers and mockVRF
    
    * fix: Lockfile
    
    * fix: Update prettier command
    
    ---------
    
    Co-authored-by: Hiroshi <108636367+0xhiroshi@users.noreply.github.com>

---
+++ test/mock/MockVRFCoordinatorV2.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract MockVRFCoordinatorV2 {
+    VRFConsumerBaseV2 public vrfConsumerBaseV2;
+    uint256 public requestCount;
+    uint256[] public requestIds;
+
+    function setVRFConsumerBaseV2(address _vrfConsumerBaseV2) external {
+        vrfConsumerBaseV2 = VRFConsumerBaseV2(_vrfConsumerBaseV2);
+    }
+
+    function requestRandomWords(
+        bytes32 keyHash,
+        uint64 subId,
+        uint16 requestConfirmations,
+        uint32 callbackGasLimit,
+        uint32 numWords
+    ) external returns (uint256 requestId) {
+        requestId = uint256(
+            keccak256(abi.encodePacked(keyHash, subId, requestConfirmations, callbackGasLimit, numWords, requestCount))
+        );
+        requestIds.push(requestId);
+        requestCount++;
+    }
+
+    function fulfillRandomWords(uint256 randomWord) external returns (uint256 requestId) {
+        uint256 requestsInQueue = requestIds.length;
+        if (requestsInQueue == 0) {
+            return 0;
+        }
+
+        requestId = requestIds[requestsInQueue - 1];
+        if (requestId == 0) return requestId;
+
+        uint256[] memory randomWords = new uint256[](1);
+        randomWords[0] = randomWord;
+
+        vrfConsumerBaseV2.rawFulfillRandomWords(requestId, randomWords);
+
+        requestIds.pop();
+    }
+}
